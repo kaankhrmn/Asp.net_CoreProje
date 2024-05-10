f50b1f0d1f6a5ff8158037e1de31e1d5b264a2c1
Kaan <kaankhrmn.61@gmail.com>
2024-05-10 22:16:14 +0300

    Yeni tema

---
+++ Asp.net_CoreProje/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net_CoreProje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
